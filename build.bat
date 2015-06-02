70ddee9e35af6bb858b6f5c81beda64ae03f13df
Clownacy <fred.arnold101@googlemail.com>
2015-06-02 14:20:16 +0100

    Moving GetFileSize.cpp

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ echo Creating directories
 if not exist bin mkdir bin
 
 echo Compiling program
-g++ -O3 kensfrontend.c KENS-source/Kosinski/GetFileSize.cpp KENS-source/Kosinski/K-Compressor.cpp KENS-source/Kosinski/K-Decompressor.cpp KENS-source/Enigma/E-Compressor.cpp KENS-source/Enigma/E-Decompressor.cpp KENS-source/Nemesis/N-Compressor.cpp KENS-source/Nemesis/N-Decompressor.cpp KENS-source/Saxman/S-Compressor.cpp KENS-source/Saxman/S-Decompressor.cpp -o bin/kensfrontend.exe
+g++ -O3 kensfrontend.c KENS-source/GetFileSize.cpp KENS-source/Kosinski/K-Compressor.cpp KENS-source/Kosinski/K-Decompressor.cpp KENS-source/Enigma/E-Compressor.cpp KENS-source/Enigma/E-Decompressor.cpp KENS-source/Nemesis/N-Compressor.cpp KENS-source/Nemesis/N-Decompressor.cpp KENS-source/Saxman/S-Compressor.cpp KENS-source/Saxman/S-Decompressor.cpp -o bin/kensfrontend.exe
 
 pause
