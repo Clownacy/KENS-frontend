bb7758c399f432f1d80170ba9a1f1e3182fa69e7
Clownacy <fred.arnold101@googlemail.com>
2015-06-05 20:13:11 +0100

    Making build script fancier
    Halo-style FTW

--- build.bat
+++ build.bat
@@ -1,26 +1,32 @@
 @ECHO off
 
-echo Creating directories
+echo|set /p=Creating directories...
 if not exist bin mkdir bin
 if not exist bin\libs mkdir bin\libs
+echo done
 
-echo Compiling program
+echo|set /p=Compiling program...
 g++ -O3 kensfrontend.c -o bin/kensfrontend.exe
+echo done
 
 echo.
-echo Copying files...
+echo Copying files
 echo.
-echo Enigma.dll
-copy KENS\Enigma.dll bin\libs
+echo|set /p=Enigma.dll...
+copy KENS\Enigma.dll bin\libs >NUL
+echo done
 echo.
-echo Kosinski.dll
-copy KENS\Kosinski.dll bin\libs
+echo|set /p=Kosinski.dll...
+copy KENS\Kosinski.dll bin\libs >NUL
+echo done
 echo.
-echo Nemesis.dll
-copy KENS\Nemesis.dll bin\libs
+echo|set /p=Nemesis.dll...
+copy KENS\Nemesis.dll bin\libs >NUL
+echo done
 echo.
-echo Saxman.dll
-copy KENS\Saxman.dll bin\libs
+echo|set /p=Saxman.dll...
+copy KENS\Saxman.dll bin\libs >NUL
+echo done
 echo.
 
 pause
