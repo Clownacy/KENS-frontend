da1896bba12ba298cb9322d8930e2dd08dcad142
Clownacy <fred.arnold101@googlemail.com>
2015-06-07 01:56:28 +0100

    Using global variables instead of constantly using argv

--- kensfrontend.c
+++ kensfrontend.c
@@ -14,12 +14,17 @@ extern "C" {
 	argv[3] = Output path
 	*/
 
+char *programpath;
+char *settings;
+char *inputpath;
+char *outputpath;
+
 char kospath[] = "libs/Kosinski.dll";
 char enipath[] = "libs/Enigma.dll";
 char nempath[] = "libs/Nemesis.dll";
 char saxpath[] = "libs/Saxman.dll";
 
-void usage ( char *programpath )
+void usage()
 {
 	char *programname = basename( programpath );
 	printf( "Frontend made by Clownacy\n\n  Usage:  %s [mode] [inputdir] [outputdir]\n\n  Modes:\n   kc - Kosinski (compress)\n   kd - Kosinski (decompress)\n   ec - Enigma (compress)\n   ed - Enigma (decompress)\n   nc - Nemesis (compress)\n   nd - Nemesis (decompress)\n   sc - Saxman (compress)\n   sd - Saxman (decompress)\n", programname );
@@ -67,81 +72,62 @@ bool initSaxmanModule()
 	return handleModuleError( SInit(saxpath), saxpath );
 }
 
-void processFile ( char *argv[1] )
+void processFile()
 {
-	if ( strcmp( argv[1], "kc" ) == 0 )
-	{
-		if ( initKosinskiModule() == false )
-			return;
-		KComp( argv[2], argv[3], false );
-	}
-	else if ( strcmp( argv[1], "kd" ) == 0 )
-	{
-		if ( initKosinskiModule() == false )
-			return;
-		KDecomp( argv[2], argv[3], 0, false );
-	}
-	else if ( strcmp( argv[1], "ec" ) == 0 )
-	{
-		if ( initEnigmaModule() == false )
-			return;
-		EComp( argv[2], argv[3], false );
-	}
-	else if ( strcmp( argv[1], "ed" ) == 0 )
-	{
-		if ( initEnigmaModule() == false )
-			return;
-		EDecomp( argv[2], argv[3], 0, false );
-	}
-	else if ( strcmp( argv[1], "nc" ) == 0 )
-	{
-		if ( initNemesisModule() == false )
-			return;
-		NComp( argv[2], argv[3] );
-	}
-	else if ( strcmp( argv[1], "nd" ) == 0 )
-	{
-		if ( initNemesisModule() == false )
-			return;
-		NDecomp( argv[2], argv[3], 0 );
-	}
-	else if ( strcmp( argv[1], "sc" ) == 0 )
-	{
-		if ( initSaxmanModule() == false )
-			return;
-		SComp( argv[2], argv[3], false );
-	}
-	else if ( strcmp( argv[1], "sd" ) == 0 )
-	{
-		if ( initSaxmanModule() == false )
-			return;
-		SDecomp( argv[2], argv[3], 0, 0 );
-	}
+	if ( strcmp( settings, "kc" ) == 0 )
+		if ( initKosinskiModule() == true )
+			KComp( inputpath, outputpath, false );
+	else if ( strcmp( settings, "kd" ) == 0 )
+		if ( initKosinskiModule() == true )
+			KDecomp( inputpath, outputpath, 0, false );
+	else if ( strcmp( settings, "ec" ) == 0 )
+		if ( initEnigmaModule() == true )
+			EComp( inputpath, outputpath, false );
+	else if ( strcmp( settings, "ed" ) == 0 )
+		if ( initEnigmaModule() == true )
+			EDecomp( inputpath, outputpath, 0, false );
+	else if ( strcmp( settings, "nc" ) == 0 )
+		if ( initNemesisModule() == true )
+			NComp( inputpath, outputpath );
+	else if ( strcmp( settings, "nd" ) == 0 )
+		if ( initNemesisModule() == true )
+			NDecomp( inputpath, outputpath, 0 );
+	else if ( strcmp( settings, "sc" ) == 0 )
+		if ( initSaxmanModule() == true )
+			SComp( inputpath, outputpath, false );
+	else if ( strcmp( settings, "sd" ) == 0 )
+		if ( initSaxmanModule() == true )
+			SDecomp( inputpath, outputpath, 0, 0 );
 	else
-		usage( argv[0] );
+		usage();
 }
 
 int main ( int argc, char *argv[1] )
 {
+	programpath = argv[0];
+	settings = argv[1];
+	inputpath = argv[2];
+	outputpath = argv[3];
+
 	switch ( argc )
 	{
 	default:
-		usage( argv[0] );
+		usage();
 		break;
 
 	#ifdef _WIN32
 		// If opened by Explorer, don't close immediately
 		case 1:
-			usage( argv[0] );
+			usage();
 			getchar();
 			break;
 	#endif
 
 	case 4:
-		if ( checkFileExist( argv[2] ) == false )
+		if ( checkFileExist( inputpath ) == false )
 			break;
 
-		processFile( argv );
+		processFile();
 	}
 
 	// Done
