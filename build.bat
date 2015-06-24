392bd03b42bad18ec67d7af76507585cdeeca2dd
Clownacy <fred.arnold101@googlemail.com>
2015-06-24 12:40:54 +0100

    Repurposing build.bat
    Will just create output directory

--- build.bat
+++ build.bat
@@ -1,32 +1,5 @@
 @ECHO off
 
-echo|set /p=Creating directories...
 if not exist bin mkdir bin
 if not exist bin\libs mkdir bin\libs
-echo done
-
-echo|set /p=Compiling program...
-make >NUL
-echo done
-
-echo.
-echo Copying files
-echo.
-echo|set /p=Enigma.dll...
-copy KENS\Enigma.dll bin\libs >NUL
-echo done
-echo.
-echo|set /p=Kosinski.dll...
-copy KENS\Kosinski.dll bin\libs >NUL
-echo done
-echo.
-echo|set /p=Nemesis.dll...
-copy KENS\Nemesis.dll bin\libs >NUL
-echo done
-echo.
-echo|set /p=Saxman.dll...
-copy KENS\Saxman.dll bin\libs >NUL
-echo done
-echo.
-
-pause
+copy KENS\*.dll bin\libs
